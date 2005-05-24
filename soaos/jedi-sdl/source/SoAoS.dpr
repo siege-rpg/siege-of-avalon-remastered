5a0d7432719981d02abfdce1d6095a67cc2f9701
savage <>
2005-05-24 22:15:02 +0000

    Latest additions to determine if it is the first time the game is run.

--- soaos/jedi-sdl/source/SoAoS.dpr
+++ soaos/jedi-sdl/source/SoAoS.dpr
@@ -1,4 +1,71 @@
 program SoAoS;
+{******************************************************************************}
+{
+  $Id$
+
+}
+{                                                                              }
+{               Siege Of Avalon : Open Source Edition                          }
+{               -------------------------------------                          }
+{                                                                              }
+{ Portions created by Digital Tome L.P. Texas USA are                          }
+{ Copyright ©1999-2000 Digital Tome L.P. Texas USA                             }
+{ All Rights Reserved.                                                         }
+{                                                                              }
+{ Portions created by Team SOAOS are                                           }
+{ Copyright (C) 2003 - Team SOAOS.                                             }
+{                                                                              }
+{                                                                              }
+{ Contributor(s)                                                               }
+{ --------------                                                               }
+{ Dominique Louis <Dominique@SavageSoftware.com.au>                            }
+{                                                                              }
+{                                                                              }
+{                                                                              }
+{ You may retrieve the latest version of this file at the SOAOS project page : }
+{   http://www.sourceforge.com/projects/soaos                                  }
+{                                                                              }
+{ The contents of this file maybe used with permission, subject to             }
+{ the GNU Lesser General Public License Version 2.1 (the "License"); you may   }
+{ not use this file except in compliance with the License. You may             }
+{ obtain a copy of the License at                                              }
+{ http://www.opensource.org/licenses/lgpl-license.php                          }
+{                                                                              }
+{ Software distributed under the License is distributed on an                  }
+{ "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or               }
+{ implied. See the License for the specific language governing                 }
+{ rights and limitations under the License.                                    }
+{                                                                              }
+{ Description                                                                  }
+{ -----------                                                                  }
+{                                                                              }
+{                                                                              }
+{                                                                              }
+{                                                                              }
+{                                                                              }
+{                                                                              }
+{                                                                              }
+{ Requires                                                                     }
+{ --------                                                                     }
+{   SDL ( http://www.libsdl.org ) and DirectX ( http://www.microsoft.com )     }
+{   Runtime libraris on Win32 and just SDL ( http://www.libsdl.org ) shared    }
+{   objects or their equivalents on Linux and other Unixes                     }
+{                                                                              }
+{ Programming Notes                                                            }
+{ -----------------                                                            }
+{   Should compile with Delphi, Kylix and FreePascal on Win32 and Linux for    }
+{   starter and FreeBSD and MacOS X etc there after.                           }
+{                                                                              }
+{                                                                              }
+{ Revision History                                                             }
+{ ----------------                                                             }
+{   September   30 2004 - DL : Initial Creation                                }
+{                                                                              }
+{
+  $Log$
+
+}
+{******************************************************************************}
 
 uses
   IniFiles,
@@ -21,13 +88,14 @@ uses
   YesNoDialog in 'interface\YesNoDialog.pas',
   SaveFile in 'engine\SaveFile.pas',
   AStar in 'ai\AStar.pas',
-  digifx in 'graphics\digifx.pas',
   DXEffects in 'graphics\DXEffects.pas',
   SiegeTypes in 'engine\SiegeTypes.pas',
   CustomAniFigure in 'engine\CustomAniFigure.pas',
   ListBoxDialog in 'interface\ListBoxDialog.pas';
 
+{$IFDEF WIN32}
 {$R *.res}
+{$ENDIF}
 
 procedure PlayOpeningMovie;
 begin
@@ -67,7 +135,10 @@ begin
     SoAoSGame.SetCaption( 'Siege of Avalon : Open Source Edition', '' );
 
     // Instantiate to get into our game loop.
-    CurrentGameInterface := TGameIntro;
+    if SoASettings.ShowIntro then
+      CurrentGameInterface := TGameIntro
+    else
+      CurrentGameInterface := TMainMenu;
 
     while CurrentGameInterface <> nil do
     begin
